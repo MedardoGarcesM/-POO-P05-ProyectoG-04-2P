f7d0d3380df866383607beed0bbbe98031b4c5d2
iesusdavila <irdavila@espol.edu.ec>
2022-01-17 08:57:15 -0500

    Se habia eliminado por error el metodo eliminar dueno, ya esta corregido ese problema

--- ProyectoG4Parcial2/src/main/java/com/mycompany/proyectog4parcial2/AdmDuenosController.java
+++ ProyectoG4Parcial2/src/main/java/com/mycompany/proyectog4parcial2/AdmDuenosController.java
@@ -113,6 +113,120 @@ public class AdmDuenosController{
 
     }
     
+    @FXML
+    private void eliminarDueno() throws IOException {
+        
+        ArrayList<Dueno> duenos = Dueno.cargarDuenos(App.pathDuenos);//cargar la lista del archivo
+        ArrayList<Ciudad> lisCiu =Ciudad.cargarCiudades(App.pathCiudades);
+        ArrayList<Mascota> mascotas = Mascota.cargarMascotas(App.pathMascotas);
+        ArrayList<Integer> listapos = new ArrayList<>();
+        
+        Dueno d = (Dueno) adDuenio.getSelectionModel().getSelectedItem();
+        //int posicion =adDuenio.getSelectionModel().getSelectedIndex();
+        int posicion=0;
+        int posicionCiu=0;
+        int posicionMascota=0;
+        int posicionDue=0;
+        int controladorPosiciones=0;
+     
+        System.out.println("Eliminando dueno");
+        for(Dueno due :duenos){
+            if(d.getCedula().equals(due.getCedula())){
+                posicion=duenos.indexOf(due);
+                System.out.println(duenos.get(posicion));
+            }
+        }
+        
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Confirmation Dialog");
+        alert.setHeaderText("Debe de confirmar la ejecucion");
+        alert.setContentText("Esta seguro que desea eliminar al dueno "+d+" con numero de cedula "+d.getCedula()+"?");
+        Optional<ButtonType> result = alert.showAndWait();
+        
+        if (result.get() == ButtonType.OK){
+            // ... user chose OK
+            //duenos.remove(posicion);
+            System.out.println(duenos.get(posicion)+" sera eliminado");
+            
+            
+            for(Mascota mascotaeliminada:mascotas){
+                if(mascotaeliminada.getDueno().getCedula().equals(duenos.get(posicion).getCedula())){
+                    listapos.add(mascotas.indexOf(mascotaeliminada));
+                }
+            }
+            
+            if(listapos.size()>0){
+                for(Integer i:listapos){
+                    mascotas.remove(i.intValue()+controladorPosiciones);
+                    controladorPosiciones--;
+                }
+            }
+            
+            
+            try {
+                FileWriter writer = new FileWriter("src/main/resources/"+App.pathMascotas);//true significa que escribe al final del archivo
+                BufferedWriter bf = new BufferedWriter(writer);
+
+                for(Mascota m1:mascotas){
+                    for(Dueno selecDu:duenos){
+                        if(selecDu.getCedula().equals(m1.getDueno().getCedula())){
+                            posicionDue=duenos.indexOf(selecDu);
+                        }
+                    }
+                    Dueno dsel = duenos.get(posicionDue);
+                    //Mascota(String id,String nombre, String tipoMascota, String raza, String fechaNacimiento, String foto, Dueno dueno)
+                    bf.write(m1.getId()+";"+m1.getNombre()+";"+m1.getTipoMascota()+";"+m1.getRaza()+";"+m1.getFechaNacimiento()+";"+m1.getFoto()+";"+dsel.getCedula());
+                    bf.newLine(); 
+                }
+
+
+                bf.close();
+                /*Alert conf = new Alert(Alert.AlertType.INFORMATION);
+                conf.setTitle("Mensaje de confirmacion");
+                conf.setHeaderText("Mascota eliminada correctamente");
+                conf.showAndWait();*/
+            
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+            
+            System.out.println(duenos.remove(posicion)+" fue eliminado");
+            
+            try {
+                FileWriter writer = new FileWriter("src/main/resources/"+App.pathDuenos);//true significa que escribe al final del archivo
+                BufferedWriter bufferedWriter = new BufferedWriter(writer);
+                
+                for(Dueno m:duenos){
+                    for(Ciudad c:lisCiu){
+                        if(m.getCedula().equals(c.getNombreC())){
+                            posicionCiu=lisCiu.indexOf(c);
+                        }
+                    }
+                    Ciudad ciu = lisCiu.get(posicionCiu);
+                    //(String cedula, String nombres, String apellidos, String direccion, String telefono, Ciudad ciudad, String email)
+                    bufferedWriter.write(m.getCedula()+","+m.getApellidos()+","+m.getNombres()+","+m.getDireccion()+","+m.getTelefono()+","+ciu+","+m.getEmail());
+                    bufferedWriter.newLine(); 
+                }
+               
+                bufferedWriter.close();
+                Alert conf = new Alert(Alert.AlertType.INFORMATION);
+                conf.setTitle("Mensaje de confirmacion");
+                conf.setHeaderText("Dueno eliminado correctamente");
+                //conf.setContentText("Dueno eliminado exitosamente");
+
+                conf.showAndWait();            
+                App.setRoot("admDuenos");
+                
+                
+            }catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            // ... user chose CANCEL or closed the
+            System.out.println(duenos.get(posicion)+" no fue eliminado");
+        }
+    }
+    
    
 
 }
